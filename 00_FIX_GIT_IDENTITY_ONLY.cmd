32ed1d3897e6df693487cab79f80ff08a4985c13
G.V. Raftogiannis FIC <gvraftogiannis@gmail.com>
2026-05-20 05:11:58 +0300

    Deploy FIC Windows Utilities site

---
+++ 00_FIX_GIT_IDENTITY_ONLY.cmd
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+git config user.name "G.V. Raftogiannis FIC"
+git config user.email "gvraftogiannis@gmail.com"
+echo Local Git identity set for this repository folder.
+pause
